7327b2fc7f4e7f53cb3add80ba6f0ad8e5d58189
takatoshi.tenko <chanten811@gmail.com>
2024-05-18 22:48:15 +0900

    misc

--- user.rb
+++ user.rb
@@ -1,9 +1,27 @@
 class User
-  def name=(name)
-    @name = name
+  attr_accessor :name, :address, :email
+
+  def profile
+    "#{name}(#{address})"
+  end
+end
+
+class MyClass
+  def method_1
+    @number = 100
   end
 
-  def name
-    @name
+  def method_2
+    @number
+  end
+end
+
+class MyClass
+  def method_1
+    @number = 100
+  end
+
+  def method_2
+    @number
   end
 end
