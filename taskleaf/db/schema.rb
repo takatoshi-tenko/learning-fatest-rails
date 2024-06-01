801d37e09ff4077b2d207516acf681a0b8964bec
takatoshi.tenko <chanten811@gmail.com>
2024-06-01 17:35:46 +0900

    fix: UserとTaskを紐づける

--- taskleaf/db/schema.rb
+++ taskleaf/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_01_055604) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_01_083211) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_01_055604) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
