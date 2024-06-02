f5974abe9233c30779e686051fceeab28cf5373c
takatoshi.tenko <chanten811@gmail.com>
2024-06-02 09:50:05 +0900

    add: 確認画面追加

--- taskleaf/app/controllers/tasks_controller.rb
+++ taskleaf/app/controllers/tasks_controller.rb
@@ -16,6 +16,11 @@ class TasksController < ApplicationController
   def create
     @task = current_user.tasks.new(task_params)
 
+    if params[:back].present?
+      render :new
+      return
+    end
+
     if @task.save
       redirect_to @task, notice: "タスク 「#{@task.name}」を登録しました"
     else
@@ -37,6 +42,11 @@ class TasksController < ApplicationController
     task = current_user.tasks.find(params[:id])
     task.destroy
     redirect_to tasks_url, notice: "タスク「#{task.name}」を削除しました。"
+  end
+
+  def confirm_new
+    @task = current_user.tasks.new(task_params)
+    render :new unless @task.valid?
   end
 
   private
